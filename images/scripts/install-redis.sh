16a303c927b9cda3a867cfa9f5db4f8b1bfac92c
han chiang <yaphc@hotmail.com>
2023-01-05 22:13:30 +0800

    remove redis version

--- images/scripts/install-redis.sh
+++ images/scripts/install-redis.sh
@@ -5,8 +5,8 @@ echo "Installing redis"
 curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
 sudo apt-get update
-sudo apt-get -y install redis-server=6:7.0.4-1rl1~focal1
-sudo apt-get -y install redis-tools=6:7.0.4-1rl1~focal1
+sudo apt-get -y install redis-server
+sudo apt-get -y install redis-tools
 
 sudo mkdir -p /etc/redis/conf.d
 sudo tee -a /etc/redis/redis.conf <<< "include /etc/redis/conf.d/redis.conf" > /dev/null
