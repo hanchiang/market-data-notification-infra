31f92e926e2219d83db62a30f2a1712d5274dcc7
han chiang <yaphc@hotmail.com>
2023-08-12 21:13:21 +0800

    Update docker installation

--- images/scripts/install-docker.sh
+++ images/scripts/install-docker.sh
@@ -4,21 +4,26 @@
 echo "Installing docker"
 
 # Remove docker
-sudo apt-get -y remove docker-desktop || true
-rm -r $HOME/.docker/desktop || true
-sudo rm /usr/local/bin/com.docker.cli || true
-sudo apt-get -y purge docker-desktop || true
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt remove $pkg; done
+sudo apt purge -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+sudo rm -rf /var/lib/docker
+sudo rm -rf /var/lib/containerd
 
+sudo apt update
 # Add docker repository
-sudo apt-get -y install ca-certificates curl gnupg lsb-release
-sudo mkdir -p /etc/apt/keyrings
+sudo apt -y install ca-certificates curl gnupg lsb-release
+
+sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo jammy)" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Install docker
-sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt update
+sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Create "docker" group, add users to allow sudo-less usage of docker
 sudo groupadd docker || true
